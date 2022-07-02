6e04c27f310747004b5e09f570a6bc43432e973e
Kushagra Sharma <kushagra.sharma111@nmims.edu.in>
2022-07-02 10:49:51 +0530

    Create ps0.py

---
+++ ps0/ps0.py
@@ -0,0 +1,10 @@
+import math
+
+# Taking user input for x and y
+x = int(input('Please enter value for x: '))
+y = int(input('Please enter value for y: '))
+
+# displaying x^y
+print(x**y)
+#displaying log x to the base 2
+print(math.log2(x))
