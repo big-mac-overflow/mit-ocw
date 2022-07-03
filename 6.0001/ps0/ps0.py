3d1a355b1292d9ca293aa6800ce28d1a95dd5968
Kushagra Sharma <kushagra.sharma111@nmims.edu.in>
2022-07-03 15:29:20 +0530

    added comments

--- 6.0001/ps0/ps0.py
+++ 6.0001/ps0/ps0.py
@@ -1,6 +1,7 @@
+# Problem set 0
 import math
 
-# Taking user input for x and y
+# Taking user input for x and y and casting into integers
 x = int(input('Please enter value for x: '))
 y = int(input('Please enter value for y: '))
 
