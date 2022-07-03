37667a21e3217edae36d21bb925d935e984e4e7c
Kushagra Sharma <kushagra.sharma111@nmims.edu.in>
2022-07-03 20:30:26 +0530

    Update lec3_strings_algos.py

--- 6.0001/code challenges/lec3_strings_algos.py
+++ 6.0001/code challenges/lec3_strings_algos.py
@@ -1,1 +1,36 @@
-print("This repo hasn't been updated yet")
+# ORIGINAL CODE BY ANA BELL
+
+an_letters = "aefhilmnorsxAEFHILMNORSX"
+word = input("I will cheer for you! Enter a word: ")
+times = int(input("Enthusiasm level (1-10): "))
+
+i = 0
+while i < len(word):
+   char = word[i]
+   if char in an_letters:
+       print("Give me an " + char + "! " + char)
+   else:
+       print("Give me a  " + char + "! " + char)
+   i += 1
+print("What does that spell?")
+for i in range(times):
+   print(word, "!!!")
+
+
+    
+# CHALLENGE: rewrite while loop with a for loop
+
+# SOLUTION
+an_letters = "aefhilmnorsxAEFHILMNORSX"
+word = input("I will cheer for you! Enter a word: ")
+times = int(input("Enthusiasm level (1-10): "))
+
+for char in word:
+   if char in an_letters:
+       print("Give me an " + char + "! " + char)
+   else:
+       print("Give me a  " + char + "! " + char)
+print("What does that spell?")
+for i in range(times):
+   print(word, "!!!")
+
